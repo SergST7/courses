62ea92631c4cad1d2fdfe43b2fb91368084c5797
Vovas11 <vovas11@users.noreply.github.com>
2016-09-09 16:20:20 +0200

    Add new classes extending User

---
+++ src/com/github/vovas11/courses/dao/Lecturer.java
@@ -0,0 +1,11 @@
+package com.github.vovas11.courses.dao;
+
+/**
+ * Represents the entity of the lecturer based on {@ User}
+ * 
+ * @author vovas11
+ * @see User
+ */
+public class Lecturer extends User {
+
+}
---
+++ src/com/github/vovas11/courses/dao/Student.java
@@ -0,0 +1,14 @@
+package com.github.vovas11.courses.dao;
+
+/**
+ * Represents the entity of the student based on {@ User}
+ * 
+ * @author vovas11
+ * @see User
+ */
+public class Student extends User {
+    
+        private String department;
+        
+        
+}
